99706d0053922a738839e23d4315536590471af8
arihantchaudhary185-collab <arihantchaudhary185@gmail.com>
2026-05-12 08:56:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("introduction to variable to JS");
+// var variable;
+//var name="arihant"
+//var name="chaudhary"
+//name = "arihant chaudhary";
+
+//console.log(name);
+
+//let variable;
+//let name ="arihant"
+//let name ="arihant";
+//name="arihant chaudhary"
+// console.log(name);
+//{
+//    var a=10;
+//   let b=20;
+//   let sum=a+b;
+//  console.log(sum);
+
+//}
+//console.log(a);
+//let name ;
+//console.log(name);
+let name = "Alice";  
+let age = 30;  
+let isstudent = true;  
+let score;  
+let car = null;  
+
+let person = { name: "Bob", age: 25 };
+
+console.log(typeof age);
